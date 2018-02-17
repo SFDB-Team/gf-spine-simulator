d69efb474c34c433204a84068a980b6aedd114cd
kjm1922 <wjdfla1922@naver.com>
2018-02-17 19:32:30 +0900

    game-plus.js 한글화
    이미지 저장 관련 자바스크립트

--- js/game-plus.js
+++ js/game-plus.js
@@ -21,7 +21,7 @@ var gvHandler = {
   },
 
   savePng : function(gameview) {
-    if (confirm("產生圖片在下方後請右鍵存檔")) {
+    if (confirm("이미지는 하단에 출력됩니다. 아래 그림을 클릭 후 이미지를 마우스 오른쪽 버튼으로 클릭해 저장할 수 있습니다. 모바일은 이미지를 길게 눌러주세요.")) {
       var renderTexture = new PIXI.RenderTexture(gameview.renderer, 1920, 1080);
       renderTexture.render(gameview.stage);
       var canvas = renderTexture.getCanvas();
