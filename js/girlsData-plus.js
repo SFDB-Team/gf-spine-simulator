cbd3c3de408b931f5ba404cac06e5f52103325a5
Jae Min Kim <wjdfla1922@naver.com>
2018-03-10 16:43:37 +0900

    typo fix

--- js/girlsData-plus.js
+++ js/girlsData-plus.js
@@ -584,7 +584,7 @@ var girlsDataKr = {
 	"Carcano1891": { //카르카노 M1891
 		"name": "\uCE74\uB974\uCE74\uB178 1891"
 	},
-	"Carcano9138": { //카르카노 M91/38
+	"Carcano1938": { //카르카노 M91/38
 		"name": "\uCE74\uB974\uCE74\uB178 M91/38"
 	},
 	"80type": { //80식
