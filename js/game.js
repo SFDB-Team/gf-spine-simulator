fba93eba0d266aadd352d5a439e1635b7e2beda0
ergo9ine <ergo9ine@gmail.com>
2018-07-15 14:16:25 +0900

    v1.8.1 bug fix/js merge

--- js/game.js
+++ js/game.js
@@ -158,6 +158,7 @@ var preview={
 		for(var i=0;i<Canilength;i++){
 			motion.push(Object.values(preview.stage.children[0].state.data.skeletonData.animations[i])[0])
 		}
+		preview.stage.off('pointerdown')
 		preview.stage.on('pointerdown', function(){
 			if(nowSkin>=Canilength){
 				preview.changeAnimation(0);
@@ -167,7 +168,7 @@ var preview={
 				nowSkin+=1
 			};
 			preview.selectAnimation.val(preview.stage.children[0].state.tracks[0].animation.name)
-    });
+		});
 	},
 
 	loadToStage:function(defaultStageData,spineData){
@@ -464,6 +465,36 @@ var gameview={
 	}
 }
 
-function onClick () {
-    console.log("hi")
-}
+var gvHandler = {
+	saveStage : function(gameview) {
+		var jsonData = {
+			'ro': [],
+			'bg': ''
+		};
+		for (i in gameview.role) {
+			jsonData.ro.push({
+				'name' : gameview.role[i].stateData.skeletonData.code,
+				'skin' : gameview.role[i].stateData.skeletonData.skin,
+				'x' : gameview.role[i].x,
+				'y' : gameview.role[i].y,
+				'scale' : gameview.role[i].scale.x,
+				'animation' : gameview.role[i].animation
+			});
+		}
+		jsonData.bg = gameview.background.filename;
+		var jsonString = JSON.stringify(jsonData);
+		var shareUrl = location.protocol+'//'+location.host+location.pathname + '#' + encodeURIComponent(jsonString);
+		alert(shareUrl);
+	},
+
+	savePng : function(gameview) {
+		if (confirm("이미지는 하단에 출력됩니다. 아래 그림을 클릭 후 이미지를 마우스 오른쪽 버튼으로 클릭해 저장할 수 있습니다. 모바일은 이미지를 길게 눌러주세요.")) {
+			var renderTexture = new PIXI.RenderTexture(gameview.renderer, 1920, 1080);
+			renderTexture.render(gameview.stage);
+			var canvas = renderTexture.getCanvas();
+			$('#saveImage').attr('src', canvas.toDataURL('image/png')).show();
+		}
+	}
+};
+
+game.setGameviewHandler(gvHandler);
