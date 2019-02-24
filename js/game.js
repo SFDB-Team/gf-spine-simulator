28b5acf1cd3db19fca2bd15fe74b01a325145bf6
ergo9ine <ergo9ine@gmail.com>
2019-02-24 16:33:14 +0900

    v2.7.2 mult language support (eng/jpn)/compoent repactoring

--- js/game.js
+++ js/game.js
@@ -1,7 +1,115 @@
+var Lang;
+if(navigator.language=="en-US"||navigator.language=="en"){
+	Lang="en-US"
+}else if(navigator.language=="ko-KR"||navigator.language=="ko"){
+	Lang="ko-KR"
+}else if(navigator.language=="ja-jp"||navigator.language=="ja"){
+	Lang="ja-JP"
+}else{
+	Lang="en-US"
+};
+const messages = {
+	"en-US": {
+		txt: {
+			title: "Girls' FrontLine Spine Simulator",
+			doll: "T-doll",
+			skin: "Costume",
+			motion: "Action",
+			select: "Please Select",
+			Gdoll: "Please Select T-doll",
+			Gmotion: "Please Select Action",
+			X: "X axis",
+			Y: "Y axis",
+			Size: "T-doll Size",
+			background: "Background",
+			AddRole: "Add",
+			StopRole1: "Play",
+			StopRole2: "Freeze",
+			Mirror: "Mirror",
+			removeRole: "Remove",
+			fpsText: "Fps Count Display",
+			none: "None",
+			RemoveAllRole: "AllRemove",
+			AllStopRole1: "AllFreeze",
+			AllStopRole2: "AllPlay",
+			savePng: "Save Image As PNG",
+			saveStage: "Url Copy",
+			saveStagetxt: "Copied URL",
+			description: "",
+		}
+	},
+	"ko-KR": {
+		txt: {
+			title: "소녀전선 숙소/치비 SD시뮬레이터",
+			doll: "인형",
+			skin: "코스튬",
+			motion: "행동",
+			select: "선택해주세요",
+			Gdoll: "인형을 선택해주세요",
+			Gmotion: "행동을 선택해주세요",
+			X: "X축",
+			Y: "Y축",
+			Size: "인형 크기",
+			background: "배경화면",
+			AddRole: "투입",
+			StopRole1: "재생",
+			StopRole2: "정지",
+			Mirror: "좌우반전",
+			removeRole: "제거",
+			fpsText: "FPS 수치 출력",
+			none: "없음",
+			RemoveAllRole: "인형 모두제거",
+			AllStopRole1: "인형 모션정지",
+			AllStopRole2: "인형 모션재생",
+			savePng: "이미지 저장",
+			saveStage: "주소 복사",
+			saveStagetxt: "복사되었으니 주소창에 붙여넣기 하세요!",
+			description: "<small>이것은 단순한 소녀전선 스파인 시뮬레이터이며, 실제 게임과는 다를 수 있습니다.</small><br>모든 이미지 파일에 대한 저작권은 게임 개발사인 <a href='http://gf.txwy.tw/' target='_blank'>天下網遊</a> 에 있습니다.<br>Orignal Simulator : <a href='https://github.com/cullus' target='_blank'>cullus</a><a href='https://github.com/cullus/gfSpinePiXi' target='_blank'>gfSpinePiXi</a><br>Original GitHub : <a href='https://github.com/wsmwason/gf-spine-simulator' target='_blank'>wsmwason/gf-spine-simulator [업데이트 오리지널]</a><br>Korean translate GitHub : <a href='https://github.com/kjm1922/gf-spine-simulator' target='_blank'>kjm1922/gf-spine-simulator [한글화 적용]</a><br>More Modifty : <a href='https://github.com/ergo9ine'>ergo9ine</a><a href='https://github.com/SFDB-Team/gf-spine-simulator' target='_blank'>/gf-spine-simulator [커스텀]</a>",
+		}
+	},
+	"ja-JP":{
+		txt:{
+			title: "ドールズフロントラインちびシミュレーター",
+			doll: "ドール",
+			skin: "コスチューム",
+			motion: "行動",
+			select: "選択してください",
+			Gdoll: "人形を選んでください",
+			Gmotion: "行動を選んでください",
+			X: "X軸",
+			Y: "Y軸",
+			Size: "大きさ",
+			background: "背景画面",
+			AddRole: "投入",
+			StopRole1: "再生",
+			StopRole2: "静止",
+			Mirror: "左右反転",
+			removeRole: "除去",
+			fpsText: "FPS 数値 出力",
+			none: "無し",
+			RemoveAllRole: "すべて削除",
+			AllStopRole1: "すべて静止",
+			AllStopRole2: "すべて再生",
+			savePng: "イメージとして保存",
+			saveStage: "URLコピー",
+			saveStagetxt: "urlがコピーされる",
+			description: "",
+		}
+	},
+};
+const i18n = new VueI18n({
+	locale: Lang, // set locale
+	fallbackLocale: "en-US",
+	messages, // set locale messages
+});
 Vue.component("form-sm",{
-	props: ["txt","id","val"],
-	template: `<div class="form-group"><label>{{ txt }}</label><select class="custom-select" :value=val @input=changes(id)>
-		<option>선택하세요</option>
+	props: ["id","val"],
+	template: `<div class="form-group">
+	<label v-if="id == 'doll'">{{ $t('txt.doll') }}</label>
+	<label v-else-if="id == 'skin'">{{ $t('txt.skin') }}</label>
+	<label v-else>{{ $t('txt.motion') }}</label>
+	<select class="custom-select" :value=val @input=changes(id)>
+		<option>{{ $t('txt.select') }}</option>
 		<optgroup v-if="id == 'doll'" v-for="(type, value) in girlsOptions" :label="value">
 			<option v-for="(name, doll) in type" :value="doll">
 				{{ girlsDataKr[doll]?girlsDataKr[doll]:name }}
@@ -26,14 +134,14 @@ Vue.component("form-sm",{
 					Motion=this.$el.nextElementSibling.nextElementSibling.querySelector("select"),
 					name=select.value,strSkinsOption="";
 				if(select.selectedIndex == 0){
-					let text="<option>선택하세요</option>";
-					Skin.innerHTML=text;
-					Motion.innerHTML=text;
+					let txt=`<option>${messages[Lang].txt.select}</option>`;
+					Skin.innerHTML=txt;
+					Motion.innerHTML=txt;
 					return
 				};
 				for(skin in girlsData[name]){
 					let skinName=skin;
-					"r"!=skin.replace(name,"").slice(0,1)&&"R"!=skin.replace(name,"").slice(0,1)||(skinName="숙소 "+skin.slice(1));
+					"r"!=skin.replace(name,"").slice(0,1)&&"R"!=skin.replace(name,"").slice(0,1)||(skinName="Dorm "+skin.slice(1));
 					strSkinsOption+=`<option value="${skin}">${skinName}</option>`
 				};
 				Skin.innerHTML=strSkinsOption;
@@ -52,14 +160,20 @@ Vue.component("form-sm",{
 	},
 });
 Vue.component("char-op",{
-	props: ["txt","id","val"],
+	props: ["id","val"],
 	template:`<div class="input-group mb-1">
-		<div class="input-group-prepend"><span class="input-group-text">{{ txt }}</span></div>
-			<select v-if="id == 'GameDoll'||id == 'GameMotion'" class="custom-select" :id=id @input=changes(id)>
-				<option v-if="id == 'GameDoll'">인형을 선택하세요</option>
-				<option v-else-if="id == 'GameMotion'">모션을 선택하세요</option>
-			</select>
-			<input v-else type="range" class="custom-range col p-0" min="0" max="800" :id=id @input=changes(id)>
+		<div class="input-group-prepend">
+			<span class="input-group-text" v-if="id == 'GameDoll'">{{ $t('txt.doll') }}</span>
+			<span class="input-group-text" v-else-if="id == 'GameMotion'">{{ $t('txt.motion') }}</span>
+			<span class="input-group-text" v-else-if="id == 'SelectposX'">{{ $t('txt.X') }}</span>
+			<span class="input-group-text" v-else-if="id == 'SelectposY'">{{ $t('txt.Y') }}</span>
+			<span class="input-group-text" v-else-if="id == 'Selectscale'">{{ $t('txt.Size') }}</span>
+		</div>
+		<select v-if="id == 'GameDoll'||id == 'GameMotion'" class="custom-select" :id=id @input=changes(id)>
+			<option v-if="id == 'GameDoll'">{{ $t('txt.Gdoll') }}</option>
+			<option v-else-if="id == 'GameMotion'">{{ $t('txt.Gmotion') }}</option>
+		</select>
+		<input v-else type="range" class="custom-range col p-0" min="0" max="800" :id=id @input=changes(id)>
 	</div>`,
 	methods: {
 		changes: function(id){
@@ -68,7 +182,7 @@ Vue.component("char-op",{
 				selectM=gameview.selectAnimation.selectedIndex;
 			if(id == "GameDoll"){
 				if(select==0){
-					gameview.selectAnimation.innerHTML="<option>모션을 선택하세요.</option>"
+					gameview.selectAnimation.innerHTML=`<option>${messages[Lang].txt.Gmotion}</option>`
 					return
 				};
 				let i,role=gameview.stage.children[select+1],TimeStopper=document.querySelector("#TimeStop"),stringAnimations="";
@@ -86,10 +200,10 @@ Vue.component("char-op",{
 				};
 				gameview.selectAnimation.innerHTML=stringAnimations;
 				if(app.timeVal[select-1]==0){
-					TimeStopper.textContent="재생";
+					TimeStopper.textContent=messages[Lang].txt.StopRole1;
 					TimeStopper.classList.add("btn-success")
 				}else{
-					TimeStopper.textContent="정지";
+					TimeStopper.textContent=messages[Lang].txt.StopRole2;
 					TimeStopper.classList.remove("btn-success")
 				}
 			}else if(id == "GameMotion"){
@@ -98,19 +212,15 @@ Vue.component("char-op",{
 				}else{
 					app.gamechangeAnimation(selectM)
 				}
-			}else if(id == "SelectposX"){
-
-			}else if(id == "SelectposY"){
-
-			}else{
-
 			}
 		},
 	},
 });
 var app = new Vue({
-		el: "#app",
-		data: {
+	i18n,
+	el: "#app",
+	data: {
+			title: messages[Lang].txt.title,
 			background: "Airport Bridge Forest IceLake Snow Street MindMap MindMap2".split(" "),
 			SelectCharacter: "",
 			SelectSkin: "",
@@ -133,8 +243,8 @@ var app = new Vue({
 				isUpdate:true,
 				selectScale:1,
 			},
-		},
-		methods: {
+	},
+	methods: {
 			changeCanvas: function(skeletonData){
 				let preview=this.preview,stringAnimations="",nowSkin=0,i;
 				preview.selectAnimation=this.$children[2].$el.querySelector("select");
@@ -255,7 +365,7 @@ var app = new Vue({
 				if(typeof n !== "number"){n=n.target.selectedIndex};
 				if(n==0&&gameview.background){
 					gameview.background.texture=PIXI.Texture.EMPTY;
-					gameview.background.filename="없음";
+					gameview.background.filename=messages[Lang].txt.none;
 					return
 				}
 				if(gameview.bgImage[n-1]){
@@ -299,7 +409,7 @@ var app = new Vue({
 				t.select();
 				document.execCommand('copy');
 				document.body.removeChild(t);
-				alert("복사되었으니 주소창에 붙여넣기 하세요!")
+				alert(messages[Lang].txt.saveStagetxt)
 			},
 			savePng: function(){
 				this.gameview.renderer.extract.canvas(this.gameview.stage).toBlob(b=>{
@@ -369,7 +479,7 @@ var app = new Vue({
 					};
 					name2=girlsOptions[i][skeletonData.code]
 				};
-				stringCharacter=`<option>${name2} (스킨:${name})</option>`;
+				stringCharacter=`<option>${name2} (${messages[Lang].txt.skin}:${name})</option>`;
 				gameview.selectCharacter.innerHTML+=stringCharacter;
 				gameview.selectCharacter.selectedIndex=gameview.role.length;
 				gameview.stage.addChild(role);
@@ -402,25 +512,26 @@ var app = new Vue({
 				let preview=this.preview,button=e.target;
 				if(preview.isUpdate){
 					preview.isUpdate=false,
-					button.textContent="재생";
+					button.textContent=messages[Lang].txt.StopRole1;
 					button.classList.toggle("btn-success")
 				}else{
 					preview.isUpdate=true,
-					button.textContent="정지";
+					button.textContent=messages[Lang].txt.StopRole2;
 					button.classList.toggle("btn-success")
 				}
 			},
 			TimeStop: function(e){
-				let gameview=this.gameview,charOn=gameview.selectCharacter.selectedIndex,button=e.target,selector=gameview.role[charOn-1].state.tracks[0];
+				let gameview=this.gameview,charOn=gameview.selectCharacter.selectedIndex;
 				if(charOn==0) return;
+				let button=e.target,selector=gameview.role[charOn-1].state.tracks[0];
 				if(selector==null) return;
 				if(selector.timeScale==1){
 					selector.timeScale=0;
-					button.textContent="재생";
+					button.textContent=messages[Lang].txt.StopRole1;
 					this.timeVal[charOn-1]=0
 				}else{
 					selector.timeScale=1;
-					button.textContent="정지";
+					button.textContent=messages[Lang].txt.StopRole2;
 					this.timeVal[charOn-1]=1
 				};
 				button.classList.toggle("btn-success")
@@ -438,16 +549,16 @@ var app = new Vue({
 				gameview.selectCharacter.remove(charOn);
 				gameview.role.splice(charOn-1,charOn);
 				gameview.focusRole=null;
-				gameview.selectAnimation.innerHTML=`<option>모션을 선택하세요.</option>`;
+				gameview.selectAnimation.innerHTML=messages[Lang].txt.StopRole1;
 				this.timeVal.splice(charOn-1,1);
-				TimeStopper.textContent="정지";
+				TimeStopper.textContent=messages[Lang].txt.StopRole2;
 				TimeStopper.classList.remove("btn-success")
 			},
 			RemoveAllRole: function(){
 				let gameview=this.gameview;
 				gameview.stage.children=gameview.stage.children.slice(0,2);
-				gameview.selectCharacter.innerHTML=`<option>인형을 선택하세요</option>`;
-				gameview.selectAnimation.innerHTML=`<option>모션이 표시됩니다.</option>`;
+				gameview.selectCharacter.innerHTML=`<option>messages[Lang].txt.Gdoll</option>`;
+				gameview.selectAnimation.innerHTML=`<option>messages[Lang].txt.Gmotion</option>`;
 				gameview.role=[];
 				gameview.focusRole=null
 			},
@@ -456,64 +567,65 @@ var app = new Vue({
 				if(charOn==0) return;
 				if(gameview.isUpdate){
 					gameview.isUpdate=false;
-					button.textContent="모두재생";
+					button.textContent=messages[Lang].txt.AllStopRole1;
 					button.classList.toggle("btn-success")
 				}else{
 					gameview.isUpdate=true;
-					button.textContent="모두정지";
+					button.textContent=messages[Lang].txt.AllStopRole2;
 					button.classList.toggle("btn-success")
 				}
 			},
-		},
-		mounted: function(){
-			let preview=this.preview,gameview=this.gameview;
-			this.girls=new Girls(`character/`);
-			preview.canvas=this.$el.querySelector(".preCanvas");
-			preview.selectX=preview.canvas.clientWidth*0.5;
-			preview.selectY=preview.canvas.clientHeight*0.85;
-			preview.stage=new PIXI.Container;
-			preview.renderer=PIXI.autoDetectRenderer(preview.canvas.clientWidth,preview.canvas.clientHeight,{transparent:true});
-			preview.lastTime=new Date().getTime();
-			preview.nowTime=new Date().getTime();
-			preview.animationFrame=window.requestAnimationFrame(this.animate);
-			preview.canvas.appendChild(preview.renderer.view);
-			gameview.canvas=this.$el.querySelector('.gameCanvas');
-			gameview.selectCharacter=this.$el.querySelector("#GameDoll");
-			gameview.selectAnimation=this.$el.querySelector("#GameMotion");
-			gameview.selectposX=this.$el.querySelector("#SelectposX");
-			gameview.selectposY=this.$el.querySelector("#SelectposY");
-			gameview.selectscale=this.$el.querySelector("#Selectscale");
-			gameview.selectX=1920/2;
-			gameview.selectY=1080/2;
-			gameview.selectposX.setAttribute("max","1920");
-			gameview.selectposY.setAttribute("max","1080");
-			gameview.selectscale.setAttribute("min","300");
-			gameview.selectscale.setAttribute("max","3000");
-			gameview.selectposX.value=gameview.selectX;
-			gameview.selectposY.value=gameview.selectY;
-			gameview.selectscale.value=gameview.selectScale*1000;
-			gameview.stage=new PIXI.Container;
-			gameview.renderer=PIXI.autoDetectRenderer(1920,1080,{transparent:true});
-			gameview.background=new PIXI.Sprite(PIXI.Texture.EMPTY);
-			gameview.stage.addChild(gameview.background);
-			gameview.lastTime=new Date().getTime();
-			gameview.nowTime=new Date().getTime();
-			gameview.fpsText=new PIXI.Text("0",{fill:"#ffffff"});
-			gameview.fpsText.x=1;
-			gameview.fpsText.y=0;
-			gameview.stage.addChild(gameview.fpsText);
-			gameview.animationFrame=window.requestAnimationFrame(this.gameAnimate);
-			gameview.canvas.appendChild(gameview.renderer.view);
-			var stageLoaded=false;
-			if(typeof defaultStageData!=="undefined"){
-				this.loadStage(defaultStageData);
-				stageLoaded=true
-			};
-			if(window.location.hash&&stageLoaded==false){
-				let hash=window.location.hash.substring(1);
-				this.loadStage(hash);
-				stageLoaded=true
-			};
-			0==stageLoaded&&(this.SelectBackground=1,this.changeBackground(1))
-		}
+	},
+	mounted: function(){
+		let preview=this.preview,gameview=this.gameview;
+		this.girls=new Girls(`character/`);
+		preview.canvas=this.$el.querySelector(".preCanvas");
+		preview.selectX=preview.canvas.clientWidth*0.5;
+		preview.selectY=preview.canvas.clientHeight*0.85;
+		preview.stage=new PIXI.Container;
+		preview.renderer=PIXI.autoDetectRenderer(preview.canvas.clientWidth,preview.canvas.clientHeight,{transparent:true});
+		preview.lastTime=new Date().getTime();
+		preview.nowTime=new Date().getTime();
+		preview.animationFrame=window.requestAnimationFrame(this.animate);
+		preview.canvas.appendChild(preview.renderer.view);
+		gameview.canvas=this.$el.querySelector('.gameCanvas');
+		gameview.selectCharacter=this.$el.querySelector("#GameDoll");
+		gameview.selectAnimation=this.$el.querySelector("#GameMotion");
+		gameview.selectposX=this.$el.querySelector("#SelectposX");
+		gameview.selectposY=this.$el.querySelector("#SelectposY");
+		gameview.selectscale=this.$el.querySelector("#Selectscale");
+		gameview.selectX=1920/2;
+		gameview.selectY=1080/2;
+		gameview.selectposX.setAttribute("max","1920");
+		gameview.selectposY.setAttribute("max","1080");
+		gameview.selectscale.setAttribute("min","300");
+		gameview.selectscale.setAttribute("max","3000");
+		gameview.selectposX.value=gameview.selectX;
+		gameview.selectposY.value=gameview.selectY;
+		gameview.selectscale.value=gameview.selectScale*1000;
+		gameview.stage=new PIXI.Container;
+		gameview.renderer=PIXI.autoDetectRenderer(1920,1080,{transparent:true});
+		gameview.background=new PIXI.Sprite(PIXI.Texture.EMPTY);
+		gameview.stage.addChild(gameview.background);
+		gameview.lastTime=new Date().getTime();
+		gameview.nowTime=new Date().getTime();
+		gameview.fpsText=new PIXI.Text("0",{fill:"#ffffff"});
+		gameview.fpsText.x=1;
+		gameview.fpsText.y=0;
+		gameview.stage.addChild(gameview.fpsText);
+		gameview.animationFrame=window.requestAnimationFrame(this.gameAnimate);
+		gameview.canvas.appendChild(gameview.renderer.view);
+		var stageLoaded=false;
+		if(typeof defaultStageData!=="undefined"){
+			this.loadStage(defaultStageData);
+			stageLoaded=true
+		};
+		if(window.location.hash&&stageLoaded==false){
+			let hash=window.location.hash.substring(1);
+			this.loadStage(hash);
+			stageLoaded=true
+		};
+		0==stageLoaded&&(this.SelectBackground=1,this.changeBackground(1));
+		document.title=this.title
+	},
 })
